4d3062ebe8aa559241f17025c627139c3931b345
Talaini <10530576+talainimohammed@users.noreply.github.com>
2024-04-09 13:08:17 +0000

    Delete MoniteurBus directory

--- MoniteurBus/src/main/java/com/monibus/moniteurbus/service/INotification.java
+++
@@ -1,8 +0,0 @@
-package com.monibus.moniteurbus.service;
-
-public interface INotification {
-
-    public void sendNotification(String message);
-    public void receiveNotification(String message);
-    
-}
